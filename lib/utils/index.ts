0e329771600c7a220b68368eb189d36efe9f101f
recepkefelii <recepkefeliii@gmail.com>
2023-03-26 07:34:24 -1200

    feat: add get server etc

---
+++ lib/utils/index.ts
@@ -0,0 +1,15 @@
+import { IExcaliServer } from '../types/app';
+
+const servers: Record<number, IExcaliServer> = {};
+
+export const getServer = (port: number): IExcaliServer => {
+  return servers[port];
+};
+
+export const setServer = (port: number, server: IExcaliServer): void => {
+  servers[port] = server;
+};
+
+export const deleteServer = (port: number): void => {
+  delete servers[port];
+};
