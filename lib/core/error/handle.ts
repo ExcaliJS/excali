c020ec0247a8ecbb341831cecf6eaf4454113429
recepkefelii <recepkefeliii@gmail.com>
2023-03-26 02:16:30 -1200

    update: add custom req error method

--- lib/core/error/handle.ts
+++ lib/core/error/handle.ts
@@ -14,4 +14,24 @@ export class ExcaliCustomError extends Error
         return new ExcaliCustomError(400, message, data)
     }
 
+    static NotAuthentified = (message: string, data = ''): ExcaliCustomError =>
+    {
+        return new ExcaliCustomError(401, message, data)
+    }
+
+    static Forbidden = (message: string, data = ''): ExcaliCustomError =>
+    {
+        return new ExcaliCustomError(403, message, data)
+    }
+
+    static NotFound = (message: string, data = ''): ExcaliCustomError => 
+    {
+        return new ExcaliCustomError(404, message, data)
+    }
+
+    static ServerError = (message: string, data = ''): ExcaliCustomError => 
+    {
+        return new ExcaliCustomError(500, message, data)
+    }
+
 }
