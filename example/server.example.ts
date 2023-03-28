45e7bd3d581b0fcfb13bd9dd56cae8a57dc96c6c
recepkefelii <recepkefeliii@gmail.com>
2023-03-28 00:46:15 -1200

    feat: add create server and router example

---
+++ example/server.example.ts
@@ -0,0 +1,14 @@
+import { Route, excaliServer } from '../lib/app/main';
+import { ExcaliCustomError } from '../lib/error/handle';
+import { ExcaliError, HttpMethods } from '../lib/type/core.type';
+import http from 'http';
+
+excaliServer(8000);
+
+Route(HttpMethods.GET, '/test', (req: http.IncomingMessage) => {
+  return 'hello world';
+});
+
+Route(HttpMethods.GET, '/deneme', (req, res) => {
+  return 'goodby world';
+});
